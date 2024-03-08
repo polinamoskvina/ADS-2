09bd8a74c3a1a5c8fa4f638c6c6ce8d22a177668
polinamoskvina <160390879+polinamoskvina@users.noreply.github.com>
2024-03-08 19:32:46 +0300

    B123.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,25 +4,44 @@
 
 
 double pown(double value, uint16_t n) {
-
+    double res = 1.0;
+    for (uint16_t i = 0; i < n; ++i) {
+        res *= value;
+    }
+    return res;
 }
 
 uint64_t fact(uint16_t n) {
-
+    if (n == 0) {
+        return 1;
+    }
+    return n * fact(n - 1);
 }
 
 double calcItem(double x, uint16_t n) {
-
+    return pown(x, n) / fact(n);
 }
 
 double expn(double x, uint16_t count) {
-
+    double sum = 0.0;
+    for (uint16_t i = 0; i < count; ++i) {
+        sum += calcItem(x, i);
+    }
+    return sum;
 }
 
 double sinn(double x, uint16_t count) {
-
+    double sum = 0.0;
+    for (uint16_t i = 0; i < count; ++i) {
+        sum += calcItem(x, 2 * i + 1) * ((i % 2 == 0) ? 1 : -1);
+    }
+    return sum;
 }
 
 double cosn(double x, uint16_t count) {
-
+    double sum = 0.0;
+    for (uint16_t i = 0; i < count; ++i) {
+        sum += calcItem(x, 2 * i) * ((i % 2 == 0) ? 1 : -1);
+    }
+    return sum;
 }
